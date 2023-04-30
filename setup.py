bf171f3efe03b7ced8610f63808709eca646a78d
Chuck Tilbury <chucktilbury@gmail.com>
2023-04-30 16:47:09 -0500

    testing

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from Cython.Build import cythonize
 
 setup(
     ext_modules=cythonize("visca.py",
-    compiler_directives = { "language_level" : "3"}),
+    compiler_directives = {"language_level" : "3"}),
 )
